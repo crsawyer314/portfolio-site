d7f632f4f1397c6f97acd11fa16affe2b42d4f38
Catherine Sawyer <crsawyer314@gmail.com>
2023-05-25 13:43:49 -0700

    routing experimentation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ChevronLeft, ChevronRight } from '@mui/icons-material';
 
 import Sidebar from "./component/Sidebar";
--- src/component/Home.js
+++ src/component/Home.js
@@ -26,10 +26,10 @@ const Home = ({ collapsed }) => {
                 <p>Technical educator, people manager, software developer, and creator</p>
               </header>
               <p>My passion is increasing diversity through code.</p>
-              <p>Whether it is grade-school students wanting to learn a new hobby or adults looking to break into tech, there is a space for everyone to learn to code. I host some personal projects on this website, including my <a href="/#/js_about">JavaScript for Adult Beginners</a>, a course targeted at adults looking to upskill and learn code.</p>
+              <p>Whether it is grade-school students wanting to learn a new hobby or adults looking to break into tech, there is a space for everyone to learn to code. I host some personal projects on this website, including my <a href="/js_about">JavaScript for Adult Beginners</a>, a course targeted at adults looking to upskill and learn code.</p>
               <p>This course is designed for those who have no prior knowledge about code or programming whatsoever, and will guide you not only through your learning journey, but also through the necessary steps to create a meaningful project for your personal portfolio.</p>
               <ul className="actions">
-                <li><a href="/#/bio" className="button big">Learn More</a></li>
+                <li><a href="/bio" className="button big">Learn More</a></li>
               </ul>
             </div>
             <span className="image object">
@@ -46,28 +46,28 @@ const Home = ({ collapsed }) => {
               <article>
                 <span className="icon fa-keyboard"></span>
                 <div className="content">
-                  <h3><a href="/#/js_about">JavaScript for Beginners</a></h3>
+                  <h3><a href="/js_about">JavaScript for Beginners</a></h3>
                   <p>A project-based JavaScript course designed for the adult novice. This link will bring you to the About page for the course.</p>
                 </div>
               </article>
               <article>
                 <span className="icon solid fa-file"></span>
                 <div className="content">
-                  <h3><a href="/#/resumes">Resumes and Interviewing</a></h3>
+                  <h3><a href="/resumes">Resumes and Interviewing</a></h3>
                   <p>This page is still under construction. When completed, it will provide templates and guides for how to successfully create a technical software resume and succeed at interviewing.</p>
                 </div>
               </article>
               <article>
                 <span className="icon solid fa-star"></span>
                 <div className="content">
-                  <h3><a href="/#/hobbies">Hobbies</a></h3>
+                  <h3><a href="/hobbies">Hobbies</a></h3>
                   <p>A place for me to display pictures of some of my favorite projects, including pyrography and quilting.</p>
                 </div>
               </article>
               <article>
                 <span className="icon solid fa-utensils"></span>
                 <div className="content">
-                  <h3><a href="/#/dinner-spinner">Dinner Spinner</a></h3>
+                  <h3><a href="/dinner-spinner">Dinner Spinner</a></h3>
                   <p>Not sure what to cook tonight? Try out this nifty page to give you some good ideas.</p>
                 </div>
               </article>
