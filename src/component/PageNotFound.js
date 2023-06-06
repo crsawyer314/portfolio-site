689ec8ddfd226334b1f4db00e621cf7f2ed3da47
Catherine Sawyer <crsawyer314@gmail.com>
2023-06-06 16:02:03 -0700

    updated 404 page and sidebar (+ styling)

--- src/component/PageNotFound.js
+++ src/component/PageNotFound.js
@@ -1,33 +1,30 @@
 import React from 'react';
 // import PropTypes from "prop-types";
-import logo from './../assets/logo.svg';
+// import logo from './../assets/logo.svg';
 
 const PageNotFound = ({ collapsed }) => {
   return (
-    <div id="main" className={collapsed ? "collapsed" : "expanded"}>
+    <div>
+      <div id="main" className={collapsed ? "collapsed" : "expanded"}>
+        <div className="inner">
+
+          {/* <!-- Header --> */}
       <header id="header">
-        <a href="index.html" className="logo">
-          <strong>Catherine Sawyer</strong>
-        </a>
+        <a href="index.html" className="logo"><strong>Catherine Sawyer</strong></a>
         <ul className="icons">
-          <li>
-            <a href="https://www.linkedin.com/in/catherine-sawyer/" className="icon brands fa-linkedin">
-              <span className="label">LinkedIn</span>
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/crsawyer314" className="icon brands fa-github">
-              <span className="label">GitHub</span>
-            </a>
-          </li>
+          <li><a href="https://www.linkedin.com/in/catherine-sawyer/" className="icon brands fa-linkedin"><span className="label">LinkedIn</span></a></li>
+          <li><a href="https://github.com/crsawyer314" className="icon brands fa-github"><span className="label">GitHub</span></a></li>
         </ul>
       </header>
+    
       <div className="content">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h2>404 Page Not Found</h2>
         <h2>If you are on this page, it may be under construction.</h2>
       </div>
     </div>
+  </div>
+</div>
   );
 }
 
--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Submenu from './Submenu';
 
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
@@ -19,41 +15,66 @@ const mod1List = [
   {title: "1.8 For loops", link: "/#/js_1.8"},
   {title: "1.9 Objects", link: "/#/js_1.9"},
   {title: "1.10 Forms and Buttons", link: "/#/js_1.10"},
-  {title: "Assessment 1", link: "/#/js_1-assessment"}
+  {title: "Assessment 1", link: "/#/js_1_assessment"}
 ];
-const mod2Title = "Module 2 - Basic Programming";
-const mod2List = [
-  {title: "2.1 Variables and data types", link: "/#/js_2.1"},
-  {title: "2.2 Mathematical and logical operators", link: "/#/js_2.2"},
-  {title: "2.3 Booleans and Conditionals (if/else statements)", link: "/#/js_2.3"},
-  {title: "2.4 Switch statements", link: "/#/js_2.4"},
-  {title: "2.5 While Loops", link: "/#/js_2.5"},
-  {title: "2.6 For Loops", link: "/#/js_2.6"},
-  {title: "Assessment 2", link: "/#/js_2-assessment"}
-];
-const mod3Title = "Module 3 - Intermediate Programming";
-const mod3List = [
-  {title: "3.1 Functions", link: "/#/js_3.1"},
-  {title: "3.2 Advanced functions", link: "/#/js_3.2"},
-  {title: "3.3 OOP", link: "/#/js_3.3"},
-  {title: "3.4 Classes, cont", link: "/#/js_3.4"},
-  {title: "3.5 Functional programming", link: "/#/js_3.5"},
-  {title: "3.6 OOP vs Functional Programming", link: "/#/js_3.6"},
-  {title: "Assessment 3", link: "/#/js_3-assessment"}
-];
-const mod4Title = "Module 4 - Advanced Programming";
-const mod4List = [
-  {title: "4.1 Recursion", link: "/#/js_4.1"},
-  {title: "4.2 Error handling", link: "/#/js_4.2"},
-  {title: "4.3 Promises", link: "/#/js_4.3"},
-  {title: "4.4 Promises, cont", link: "/#/js_4.4"},
-  {title: "4.5 Async/Await", link: "/#/js_4.5"},
-  {title: "Assessment 4", link: "/#/js_4-assessment"}
-];
+// const mod2Title = "Module 2 - Basic Programming";
+// const mod2List = [
+//   {title: "2.1 Variables and data types", link: "/#/js_2.1"},
+//   {title: "2.2 Mathematical and logical operators", link: "/#/js_2.2"},
+//   {title: "2.3 Booleans and Conditionals (if/else statements)", link: "/#/js_2.3"},
+//   {title: "2.4 Switch statements", link: "/#/js_2.4"},
+//   {title: "2.5 While Loops", link: "/#/js_2.5"},
+//   {title: "2.6 For Loops", link: "/#/js_2.6"},
+//   {title: "Assessment 2", link: "/#/js_2_assessment"}
+// ];
+// const mod3Title = "Module 3 - Intermediate Programming";
+// const mod3List = [
+//   {title: "3.1 Functions", link: "/#/js_3.1"},
+//   {title: "3.2 Advanced functions", link: "/#/js_3.2"},
+//   {title: "3.3 OOP", link: "/#/js_3.3"},
+//   {title: "3.4 Classes, cont", link: "/#/js_3.4"},
+//   {title: "3.5 Functional programming", link: "/#/js_3.5"},
+//   {title: "3.6 OOP vs Functional Programming", link: "/#/js_3.6"},
+//   {title: "Assessment 3", link: "/#/js_3_assessment"}
+// ];
+// const mod4Title = "Module 4 - Advanced Programming";
+// const mod4List = [
+//   {title: "4.1 Recursion", link: "/#/js_4.1"},
+//   {title: "4.2 Error handling", link: "/#/js_4.2"},
+//   {title: "4.3 Promises", link: "/#/js_4.3"},
+//   {title: "4.4 Promises, cont", link: "/#/js_4.4"},
+//   {title: "4.5 Async/Await", link: "/#/js_4.5"},
+//   {title: "Assessment 4", link: "/#/js_4_assessment"}
+// ];
 
 const Sidebar = ({ collapsed, toggleSidebar }) => {
 
   const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
+  const [isPocketDrawerOpen, setIsPocketDrawerOpen] = useState(false);
+  const [isPocketDrawerOpen1, setIsPocketDrawerOpen1] = useState(false);
+  // const [isPocketDrawerOpen2, setIsPocketDrawerOpen2] = useState(false);
+  // const [isPocketDrawerOpen3, setIsPocketDrawerOpen3] = useState(false);
+  // const [isPocketDrawerOpen4, setIsPocketDrawerOpen4] = useState(false);
+
+  const handlePocketDrawerToggle = () => {
+    setIsPocketDrawerOpen(!isPocketDrawerOpen);
+  };
+
+  const handlePocketDrawerToggle1 = () => {
+    setIsPocketDrawerOpen1(!isPocketDrawerOpen1);
+  };
+
+  // const handlePocketDrawerToggle2 = () => {
+  //   setIsPocketDrawerOpen2(!isPocketDrawerOpen2);
+  // };
+
+  // const handlePocketDrawerToggle3 = () => {
+  //   setIsPocketDrawerOpen3(!isPocketDrawerOpen3);
+  // };
+
+  // const handlePocketDrawerToggle4 = () => {
+  //   setIsPocketDrawerOpen4(!isPocketDrawerOpen4);
+  // };
 
   useEffect(() => {
     const getWindowWidth = () => {
@@ -92,7 +113,7 @@ const Sidebar = ({ collapsed, toggleSidebar }) => {
           <header className="major">
             <Typography>Menu</Typography>
           </header>
-          <ul>
+          <ul className="sidebar-menu">
             <li>
               <Typography>
                 <a href="/#/home">Homepage</a>
@@ -104,42 +125,105 @@ const Sidebar = ({ collapsed, toggleSidebar }) => {
               </Typography>
             </li>
             <li>
-              <Accordion>
-                <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+              <div className="sidebar-item-button" onClick={handlePocketDrawerToggle}>
+                <span className="sidebar-item-title">
                   <Typography>JavaScript for Beginners</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
+                  <ExpandMoreIcon className="expand-icon" style={{ transform: isPocketDrawerOpen ? 'rotate(180deg)' : 'rotate(0)' }} />
+                </span>
+              </div>
+              {isPocketDrawerOpen && (
+                <div className="submenu">
                   <ul>
+                    <li><Typography><a href="/js_about">About</a></Typography></li>
+                    <li><Typography><a href="/js_syllabus">Syllabus</a></Typography></li>
+                    <li><Typography><a href="/resumes">Resumes and Interviewing</a></Typography></li>
                     <li>
-                      <Typography>
-                        <a href="/#/js_about">About</a>
-                      </Typography>
+                      <div onClick={handlePocketDrawerToggle1}>
+                        <span className="sidebar-item-title">
+                          <Typography>{mod1Title}
+                          <ExpandMoreIcon className="expand-icon" style={{ transform: isPocketDrawerOpen1 ? 'rotate(180deg)' : 'rotate(0)' }} />
+                          </Typography>
+                        </span>
+                        
+                      </div>
+                      {isPocketDrawerOpen1 && (
+                        <div className="submenu">
+                          <ul>
+                            {mod1List.map((item, index) => (
+                              <li key={index}>
+                                <Typography><a href={item.link}>{item.title}</a></Typography>
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      )}
                     </li>
-                    <li>
-                      <Typography>
-                        <a href="/#/js_syllabus">Syllabus</a>
-                      </Typography>
-                    </li>
-                    <li>
-                      <Submenu title={mod1Title} itemList={mod1List} />
-                    </li>
-                    <li>
-                      <Submenu title={mod2Title} itemList={mod2List} />
-                    </li>
-                    <li>
-                      <Submenu title={mod3Title} itemList={mod3List} />
-                    </li>
-                    <li>
-                      <Submenu title={mod4Title} itemList={mod4List} />
-                    </li>
+                    {/* <li>
+                      <div onClick={handlePocketDrawerToggle2}>
+                        <span className="sidebar-item-title">
+                        <Typography>{mod2Title}
+                          <ExpandMoreIcon className="expand-icon" style={{ transform: isPocketDrawerOpen2 ? 'rotate(180deg)' : 'rotate(0)' }} />
+                        </Typography>
+                        </span>
+                        
+                      </div>
+                      {isPocketDrawerOpen2 && (
+                        <div className="submenu">
+                          <ul>
+                            {mod2List.map((item, index) => (
+                              <li key={index}>
+                                <Typography><a href={item.link}>{item.title}</a></Typography>
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      )}
+                    </li> */}
+                    {/* <li>
+                      <div onClick={handlePocketDrawerToggle3}>
+                        <span className="sidebar-item-title">
+                        <Typography>{mod3Title}
+                          <ExpandMoreIcon className="expand-icon" style={{ transform: isPocketDrawerOpen3 ? 'rotate(180deg)' : 'rotate(0)' }} />
+                        </Typography>
+                        </span>
+                        
+                      </div>
+                      {isPocketDrawerOpen3 && (
+                        <div className="submenu">
+                          <ul>
+                            {mod3List.map((item, index) => (
+                              <li key={index}>
+                                <Typography><a href={item.link}>{item.title}</a></Typography>
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      )}
+                    </li> */}
+                    {/* <li>
+                      <div onClick={handlePocketDrawerToggle4}>
+                        <span className="sidebar-item-title">
+                        <Typography>{mod4Title}
+                          <ExpandMoreIcon className="expand-icon" style={{ transform: isPocketDrawerOpen4 ? 'rotate(180deg)' : 'rotate(0)' }} />
+                        </Typography>
+                        </span>
+                        
+                      </div>
+                      {isPocketDrawerOpen4 && (
+                        <div className="submenu">
+                          <ul>
+                            {mod4List.map((item, index) => (
+                              <li key={index}>
+                                <Typography><a href={item.link}>{item.title}</a></Typography>
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      )}
+                    </li> */}
                   </ul>
-                </AccordionDetails>
-              </Accordion>
-            </li>
-            <li>
-              <Typography>
-                <a href="/#/resumes">Resumes and Interviewing</a>
-              </Typography>
+                </div>
+              )}
             </li>
             <li>
               <Typography>
@@ -153,7 +237,7 @@ const Sidebar = ({ collapsed, toggleSidebar }) => {
             </li>
           </ul>
         </nav>
-
+  
         <footer id="footer">
           <Typography className="copyright">
             &copy; Catherine Sawyer. All rights reserved.
